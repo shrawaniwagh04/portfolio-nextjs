fa07b9e7e6caa8e299409f4bb84367a3e7c972d7
Shrawani <shrawani.wagh@coditas.com>
2025-06-12 22:54:38 +0530

    feat/contact-contact-form-added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
-import WithHeaderFooter from '../hoc/WithHeaderFooter/WithHeaderFooter'
+import WithHeaderFooter from "../hoc/WithHeaderFooter/WithHeaderFooter";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <WithHeaderFooter headerData={[]}>
       <Component {...pageProps} />
+      <ToastContainer position="top-right" autoClose={3000} />
     </WithHeaderFooter>
   );
 }
