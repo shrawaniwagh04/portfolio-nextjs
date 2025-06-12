1a1b347f7fe291013b8f4b3bdaa912920688cbe9
Shrawani <shrawani.wagh@coditas.com>
2025-06-12 20:24:24 +0530

    feat/about-about-section-added

--- src/views/About/About.tsx
+++ src/views/About/About.tsx
@@ -1,11 +1,73 @@
-import styles from "./About.module.scss" 
-import type { AboutProps } from "./About.types.ts" 
+import { useEffect, useState } from "react";
+import { AboutData, skillsData, SubNavList } from "./About.data";
+import styles from "./About.module.scss";
+import type { AboutProps } from "./About.types";
 import { usePathname } from "next/navigation";
- 
-const About = ({}: AboutProps) => { 
-    const path = usePathname();
-    console.log(path)
-    return <div>ABout</div>; 
-}; 
- 
-export default About 
+import useWindowWidth from "@/hooks/useWindowWidth";
+import { isWeakMap } from "util/types";
+
+const About = ({}: AboutProps) => {
+  const [currentNav, setCurrentNav] = useState(SubNavList[0]?.id || 0);
+  const { isMobileView } = useWindowWidth();
+  const [data, setData] = useState(SubNavList[0]);
+
+  useEffect(() => {
+    const selectedData = SubNavList.find((item) => item.id === currentNav);
+    setData(selectedData);
+  }, [currentNav]);
+
+  return (
+    <div className={styles.AboutContainer}>
+      <div className={styles.About}>
+        <h2>{AboutData.title}</h2>
+        <p>{AboutData.body}</p>
+      </div>
+
+      <span className={styles.AboutSpan}></span>
+
+      <div className={styles.AboutNavigation}>
+        <nav className={styles.AboutSubNav}>
+          {SubNavList.map((item) => (
+            <li
+              key={item.id}
+              onClick={() => setCurrentNav(item.id)}
+              className={currentNav === item.id ? styles.active : ""}
+            >
+              {item.work}
+            </li>
+          ))}
+        </nav>
+      </div>
+
+      <div className={styles.AboutContent}>
+        <h3>{data.title}</h3>
+
+        <ul>
+          {data.description.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+
+        <div className={styles.AboutSkillSection}>
+          <h4>Skills</h4>
+          <div
+            className={isMobileView ? styles.AboutTagsMobile : styles.AboutTags}
+          >
+            {data.skills.map((item) => (
+              <div className={styles.Tag}>{item}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <h2>Skills</h2>
+      <div className={isMobileView ? styles.AboutTagsMobile : styles.AboutTags}>
+        {skillsData.map((item) => (
+          <li className={styles.Tag}>{item}</li>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/views/Projects/Projects.tsx
+++ src/views/Projects/Projects.tsx
@@ -1,10 +1,12 @@
 "use client";
-import Card from "@/components/Card/Card";
 import styles from "./Projects.module.scss";
 import type { ProjectsProps } from "./Projects.types.ts";
 import { Project } from "@/constants/Project/Project.constant";
 import useWindowWidth from "@/hooks/useWindowWidth";
+import dynamic from "next/dynamic";
 import { useEffect } from "react";
+
+const Card = dynamic(() => import("@/components/Card/Card"));
 
 const Projects = ({}: ProjectsProps) => {
   const { isMobileView } = useWindowWidth();
@@ -16,9 +18,9 @@ const Projects = ({}: ProjectsProps) => {
     <div className={styles.ProjectContainer}>
       <div
         className={
-          isMobileView? styles.ProjectsSubContainerMobileView
-            :styles.ProjectsSubContainer
-             
+          isMobileView
+            ? styles.ProjectsSubContainerMobileView
+            : styles.ProjectsSubContainer
         }
       >
         {Project.map((item) => {
