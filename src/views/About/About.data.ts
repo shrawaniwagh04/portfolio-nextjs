980564456ac74d8bcc301f9a8390fb29141d6b3c
shrawaniwagh04 <shrawani.wagh@coditas.com>
2025-06-12 21:08:30 +0530

    Feat/about-added-about-section (#3)
    * feat/about-about-section-added
    
    * feat/about-about-section-added

---
+++ src/views/About/About.data.ts
@@ -0,0 +1,81 @@
+//About Data 
+export const AboutData = {
+  title: "About",
+  body:
+    "I’m a final-year Computer Engineering student at Bajaj Institute of Technology (2021–2025) with hands-on experience in React, React Native, JavaScript, and Firebase. I've interned at Coditas and Indigies Innovation, building scalable web and mobile apps, and collaborating on real-world projects using tools like Git, Redux, and Azure DevOps. As a community contributor and student leader, I’ve developed event platforms, led design teams, and earned recognition in national-level competitions for innovation and impact."
+};
+
+
+export const SubNavList = [
+  {
+    id: 1,
+    work: "Coditas",
+    title: "Associate Software Engineering Intern",
+    description: [
+      "Receiving structured training in HTML, CSS, and JavaScript, with a strong emphasis on writing clean, maintainable code.",
+      "Gained hands-on experience with Git, mastering version control and collaborative development workflows.",
+      "Preparing for advanced training in React, focusing on building scalable and high-performance web applications.",
+    ],
+    skills: ["HTML", "CSS", "JavaScript", "Git", "React"],
+  },
+
+  {
+    id: 2,
+    work: "Indigies",
+    title: "Software Engineering Intern",
+    description: [
+      "Developed the front-end of two React Native mobile apps during an internship, focusing on creating an effective and user-friendly UI.",
+      "Conducted 3 POCs to assess feasibility and explore technical solutions, leading to successful project execution.",
+      "Utilized Redux, Swagger, and Azure DevOps for state management, API testing, and task tracking.",
+    ],
+    skills: ["React Native", "Redux", "Swagger", "Azure DevOps", "UI Design"],
+  },
+
+  {
+    id: 3,
+    work: "WTM",
+    title: "Women Techmakers Community ",
+    description: [
+      "Contributed to developing the community website using React.",
+      "Assisted in organizing events and improving the overall online presence of the community.",
+    ],
+    skills: ["React", "Team Collaboration", "Community Events"],
+  },
+
+  {
+    id: 4,
+    work: "GDSC",
+    title: "Graphic Design Lead",
+    description: [
+      "Led the design team, increasing engagement by 25% through creative visual campaigns.",
+      "Designed the community website and brand assets, maintaining consistency across platforms.",
+    ],
+    skills: ["Graphic Design", "Figma", "Leadership", "Branding", "Web Design"],
+  },
+];
+
+
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "SCSS",
+  "JavaScript",
+  "React",
+  "React Native",
+  "Tailwind CSS",
+  "Redux",
+  "Firebase",
+  "Git",
+  "Azure DevOps",
+  "Swagger",
+  "REST APIs",
+  "UI/UX",
+  "Responsive Design",
+  "Team Collaboration",
+  "Graphic Design",
+  "Figma",
+  "Branding",
+  "Community Engagement",
+  "Version Control"
+];
