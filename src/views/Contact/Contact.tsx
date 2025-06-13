010eb8751fbe469c78c71be431d3ada23cfddee8
shrawaniwagh04 <shrawani.wagh@coditas.com>
2025-06-13 16:51:33 +0530

    Feat/contact (#4)
    * feat/about-about-section-added
    
    * feat/about-about-section-added
    
    * feat/contact-added-contact-form
    
    * feat/contact-contact-form-added
    
    * feat/contact-contact-form-added

--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -1,11 +1,46 @@
-import { usePathname } from "next/navigation";
-import styles from "./Contact.module.scss" 
-import type { ContactProps } from "./Contact.types.ts" 
- 
-const Contact = ({}: ContactProps) => { 
-        const path = usePathname();
-        console.log(path)
-    return <div>CONTACT</div>; 
-}; 
- 
-export default Contact 
+"use client";
+
+import styles from "./Contact.module.scss";
+import emailjs from "@emailjs/browser";
+import { contactFields } from "./Contact.data";
+import { toast } from "react-toastify";
+import dynamic from "next/dynamic";
+
+const Form = dynamic(() => import("@/components/Form/Form"));
+
+export default function Contact() {
+  const handleFormSubmit = (formData: Record<string, string>) => {
+    const templateParams = {
+      name: formData.name,
+      to_name: "Shrawani Wagh",
+      message: formData.message,
+      email: formData.email,
+    };
+
+    emailjs
+      .send(
+        "service_okwxx5a",
+        "template_foivzxo",
+        templateParams,
+        "MOnURdGimrQSPUJfQ"
+      )
+      .then(
+        (result) => {
+          toast.success("Message sent successfully!");
+        },
+        (error) => {
+          console.error("Error sending email:", error.text);
+          toast.error("Failed to send message. Please try again later.");
+        }
+      );
+  };
+
+  return (
+    <div className={styles.ContactContainer}>
+      <div className={styles.ContactContent}>
+        <h2>Contact Me</h2>
+        <Form data={contactFields} onSubmit={handleFormSubmit} />
+      </div>
+    </div>
+  );
+}
